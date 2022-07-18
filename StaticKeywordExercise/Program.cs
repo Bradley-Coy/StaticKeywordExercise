a267328c9c49f8130142d9f801e80586d603e49c
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-18 07:41:54 -0700

    first commit - exercise 1

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿using StaticKeywordExercise;
+
+Console.WriteLine("Fahrenheit -> Celsius");
+Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+
+Console.WriteLine("Celsius -> Fahrenheit");
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticKeywordExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            return (fTemp - 32) * 5 / 9;
+
+        }
+
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            return (cTemp * 9) / 5 + 32;
+
+        }
+    }
+}
+
